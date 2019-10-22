fa1c6cb3df8f36704bbcb80566a8a608e84b3cba
Nikolas Moya <moya@work.co>
2019-10-22 16:24:40 +0200

    Cartpole improvements

--- agents/cartpole/cartpole-qtable.py
+++ agents/cartpole/cartpole-qtable.py
@@ -1,6 +1,7 @@
 import gym, math, time
 from gym import wrappers
 import numpy as np
+from collections import deque
 
 class QLearn():
   def __init__(self, observation_space, action_space):
@@ -9,7 +10,7 @@ class QLearn():
     self.min_alpha = 0.1  # learning rate [0, 1]
     self.observation_space = observation_space
     self.action_space = action_space
-    self.nbr_states = env.observation_space.shape[0]
+    self.nbr_states = observation_space.shape[0]
     self.nbr_actions = action_space.n
     # creates a 5-d matrix to store all 72 states with 2 actions each:
     # self.Q[0][0][0][0] => [0, 0]
@@ -44,24 +45,27 @@ class QLearn():
     self.Q[state][action] = self.Q[state][action] + \
       alpha * (reward + self.gamma * value_new_state - self.Q[state][action])
 
+def cartpole():
+  # outdir = './videos/'
+  env = gym.make('CartPole-v0')
+  # env = wrappers.Monitor(env, outdir, force=True)
+  agent = QLearn(env.observation_space, env.action_space)
+  past_rewards = deque(maxlen=100)
+  for i_episode in range(500):
+    state = agent.discretize(env.reset())
+    current_reward = 0
+    for t in range(200):
+      env.render()
+      action = agent.decide_action(state, i_episode)
+      new_state, reward, done, _ = env.step(action)
+      new_state = agent.discretize(new_state)
+      agent.update(state, action, reward, new_state, i_episode)
+      state = new_state
+      current_reward += reward
+    
+    past_rewards.append(reward)
+    print("Eps: {:04d} Steps: {:04d} Reward: {:3.2f} Avg-Reward: {:3.2f} Explo: {:.3f}".format(
+        i_episode, t+1, current_reward, np.average(past_rewards), agent.gamma))
+  env.close()
 
-outdir = './videos/'
-env = gym.make('CartPole-v0')
-# env = wrappers.Monitor(env, outdir, force=True)
-agent = QLearn(env.observation_space, env.action_space)
-for i_episode in range(500):
-  # time.sleep(0.5)
-  state = agent.discretize(env.reset())
-  for t in range(200):
-    env.render()
-    action = agent.decide_action(state, i_episode)
-    new_state, reward, done, _ = env.step(action)
-    new_state = agent.discretize(new_state)
-    agent.update(state, action, reward, new_state, i_episode)
-    state = new_state
-    if t == 199:
-      print("WON!!!", i_episode)
-    if done:
-      print("Episode {} finished after {} timesteps".format(i_episode, t+1))
-      break
-env.close()
+cartpole()
