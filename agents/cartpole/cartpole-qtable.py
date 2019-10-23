e80e2779e4e158b2cbb913153c5c64767a7bedfb
Nikolas Moya <moya@work.co>
2019-10-23 15:23:55 +0200

    Improvements to Q-table cartpole

--- agents/cartpole/cartpole-qtable.py
+++ agents/cartpole/cartpole-qtable.py
@@ -1,17 +1,44 @@
-import gym, math, time
+from gym import envs
+import gym, math
 from gym import wrappers
 import numpy as np
 from collections import deque
 
+# Observation
+# idx 	Observation 	Min 	Max
+# 0 	Cart Position - 2.4 	2.4
+# 1 	Cart Velocity - Inf 	Inf
+# 2 	Pole Angle 	~ -41.8° 	~ 41.8°
+# 3 	Pole Velocity At Tip - Inf 	Inf
+
+# Actions
+# Num 	Action
+# 0 	Push cart to the left
+# 1 	Push cart to the right
+
+# Reward
+# Reward is 1 for every step taken, including the termination step
+
+# Episode Termination
+# Pole Angle is more than ±12°
+# Cart Position is more than ±2.4 (center of the cart reaches the edge of the display)
+# Episode length is greater than 200
+
+# Try with keyboard: 1 Push cart to the left, 2 Push cart to the right
+# python examples/agents/keyboard_agent.py CartPole-v0
+
 class QLearn():
   def __init__(self, observation_space, action_space):
     self.buckets = (1, 1, 6, 12)
-    self.gamma = 1.0  # discount factor [0, 1]
-    self.min_alpha = 0.1  # learning rate [0, 1]
+    self.gamma = 1.0 # discount factor [0, 1]
+    self.alpha = 0.1  # learning rate [0, 1]
     self.observation_space = observation_space
     self.action_space = action_space
     self.nbr_states = observation_space.shape[0]
     self.nbr_actions = action_space.n
+    self.min_epsilon = 0.1
+    self.epsilon = 1.0
+    self.epsilon_decay = 0.9995
     # creates a 5-d matrix to store all 72 states with 2 actions each:
     # self.Q[0][0][0][0] => [0, 0]
     self.Q = np.zeros(self.buckets + (action_space.n,))
@@ -34,13 +61,12 @@ class QLearn():
     # argmax = index with max value in an array
     # since the each index represents an action
     # so the index with max value is the best action
-    epsilon = max(
-        0.1, min(1, 1.0 - math.log10((episode + 1) / 25)))
-    return self.action_space.sample() if (np.random.random() <= epsilon) else np.argmax(self.Q[state])
+    self.epsilon = max(self.min_epsilon, self.epsilon * self.epsilon_decay)
+    return self.action_space.sample() if (np.random.random() <= self.epsilon) else np.argmax(self.Q[state])
 
-  def update(self, state, action, reward, new_state, episode):
+  def update(self, state, action, reward, new_state, done, episode):
     value_new_state = np.max(self.Q[new_state])
-    alpha = max(self.min_alpha, min(
+    alpha = max(self.alpha, min(
         1.0, 1.0 - math.log10((episode + 1) / 25)))
     self.Q[state][action] = self.Q[state][action] + \
       alpha * (reward + self.gamma * value_new_state - self.Q[state][action])
@@ -55,17 +81,21 @@ def cartpole():
     state = agent.discretize(env.reset())
     current_reward = 0
     for t in range(200):
-      env.render()
+      # if i_episode > 300:
+      # env.render()
       action = agent.decide_action(state, i_episode)
       new_state, reward, done, _ = env.step(action)
       new_state = agent.discretize(new_state)
-      agent.update(state, action, reward, new_state, i_episode)
+      agent.update(state, action, reward, new_state, done, i_episode)
       state = new_state
       current_reward += reward
+      if done:
+        break
     
-    past_rewards.append(reward)
+    past_rewards.append(current_reward)
     print("Eps: {:04d} Steps: {:04d} Reward: {:3.2f} Avg-Reward: {:3.2f} Explo: {:.3f}".format(
-        i_episode, t+1, current_reward, np.average(past_rewards), agent.gamma))
+        i_episode, t+1, current_reward, np.average(past_rewards), agent.epsilon))
   env.close()
+
 
 cartpole()
