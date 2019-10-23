aa5616d982a7289fb4235f55a1361aaf05c4026c
Nikolas Moya <moya@work.co>
2019-10-23 22:06:47 +0200

    Final touches for cartpole qtable

--- agents/cartpole/cartpole-qtable.py
+++ agents/cartpole/cartpole-qtable.py
@@ -29,47 +29,59 @@ from collections import deque
 
 class QLearn():
   def __init__(self, observation_space, action_space):
-    self.buckets = (1, 1, 6, 12)
+    self.states_per_obs = (1, 1, 6, 12)
     self.gamma = 1.0 # discount factor [0, 1]
     self.alpha = 0.1  # learning rate [0, 1]
     self.observation_space = observation_space
     self.action_space = action_space
-    self.nbr_states = observation_space.shape[0]
-    self.nbr_actions = action_space.n
     self.min_epsilon = 0.1
     self.epsilon = 1.0
     self.epsilon_decay = 0.9995
     # creates a 5-d matrix to store all 72 states with 2 actions each:
     # self.Q[0][0][0][0] => [0, 0]
-    self.Q = np.zeros(self.buckets + (action_space.n,))
+    self.Q = np.zeros(self.states_per_obs + (action_space.n,))
+
+  def normalize(self, value, lower_bound, upper_bound):
+    # Returns a value within [-1, 1]
+    return (value + abs(lower_bound)) / (upper_bound - lower_bound)
+
+  def linear_transform(self, value, max_value):
+    # raw_value is a value within [-max_value, max_value]
+    raw_value = int(round((max_value - 1) * value))
+    # Will return a number within [0, max_value-1]
+    return min((max_value - 1), max(0, raw_value))
 
   def discretize(self, obs):
     upper_bounds = [self.observation_space.high[0], 0.5,
                     self.observation_space.high[2], math.radians(50)]
     lower_bounds = [self.observation_space.low[0], -0.5,
                     self.observation_space.low[2], -math.radians(50)]
-    ratios = [(obs[i] + abs(lower_bounds[i])) /
-              (upper_bounds[i] - lower_bounds[i]) for i in range(len(obs))]
-    new_obs = [int(round((self.buckets[i] - 1) * ratios[i]))
+    ratios = [self.normalize(obs[i], lower_bounds[i], upper_bounds[i])
               for i in range(len(obs))]
-    new_obs = [min(self.buckets[i] - 1, max(0, new_obs[i]))
-              for i in range(len(obs))]
-    # discretize all the atributes within [0, bucket[i]]
+    new_obs = [self.linear_transform(
+        ratios[i], self.states_per_obs[i]) for i in range(len(obs))]
     return tuple(new_obs)
 
   def decide_action(self, state, episode):
     # argmax = index with max value in an array
     # since the each index represents an action
     # so the index with max value is the best action
+    if (np.random.random() <= self.epsilon):
+      action = self.action_space.sample()
+    else:
+      action = np.argmax(self.Q[state])
     self.epsilon = max(self.min_epsilon, self.epsilon * self.epsilon_decay)
-    return self.action_space.sample() if (np.random.random() <= self.epsilon) else np.argmax(self.Q[state])
+    return action
+
+  def alpha_fn(self, x):
+    return max(self.alpha, min(
+        1.0, 1.0 - math.log10((x+1) / 25)))
 
   def update(self, state, action, reward, new_state, done, episode):
-    value_new_state = np.max(self.Q[new_state])
-    alpha = max(self.alpha, min(
-        1.0, 1.0 - math.log10((episode + 1) / 25)))
+    value_next_state = np.max(self.Q[new_state])
+    alpha = self.alpha_fn(episode)
     self.Q[state][action] = self.Q[state][action] + \
-      alpha * (reward + self.gamma * value_new_state - self.Q[state][action])
+      alpha * (reward + self.gamma * value_next_state - self.Q[state][action])
 
 def cartpole():
   # outdir = './videos/'
@@ -93,9 +105,8 @@ def cartpole():
         break
     
     past_rewards.append(current_reward)
-    print("Eps: {:04d} Steps: {:04d} Reward: {:3.2f} Avg-Reward: {:3.2f} Explo: {:.3f}".format(
-        i_episode, t+1, current_reward, np.average(past_rewards), agent.epsilon))
+    print("Eps: {:04d} Reward: {:3.2f} Avg-Reward: {:3.2f} Explo: {:.3f}".format(
+        i_episode, current_reward, np.average(past_rewards), agent.epsilon))
   env.close()
-
 
 cartpole()
